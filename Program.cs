ebdbc726e2326a54486b01ae2047eb16dce818dd
Akankshi Priya <akankshipriya24@gmail.com>
2022-08-25 09:01:18 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,18 +10,18 @@ namespace Training
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Do you want to continue");
-            string abc = Console.ReadLine();
+            string abc;
 
             do
             {
                 Console.WriteLine("Press 1 to enter student data ");
                 Console.WriteLine("Press 2 to display the data");
                 int input = int.Parse(Console.ReadLine());
-                int studentID;
-                string studentName;
-                string studentCourse;
-                int studentMarks;
+                int studentID = 0;
+                string studentName = "";
+                string studentCourse = "";
+                int studentMarks = 0;
+                int num, rem, temp, sum = 0;
 
                 if (input == 1)
                 {
@@ -34,14 +34,57 @@ namespace Training
                     Console.WriteLine("Enter student Marks ");
                     studentMarks = int.Parse(Console.ReadLine());
                 }
-                else
+                else if (input == 2)
                 {
-                    Console.WriteLine("ID :", studentID);
-                    Console.WriteLine("ID :", studentName);
-                    Console.WriteLine("ID :", studentCourse);
-                    Console.WriteLine("ID :", studentMarks);
+                    Console.WriteLine("ID : " + studentID);
+                    Console.WriteLine("Name :" + studentName);
+                    Console.WriteLine("Course :" + studentCourse);
+                    Console.WriteLine("Marks :" + studentMarks);
 
                 }
+                else if (input == 3)
+                {
+                    Console.Write("Enter the Number to check palindrome: ");
+                    num = int.Parse(Console.ReadLine());
+                    temp = num;
+                    while (num > 0)
+                    {
+                        rem = num % 10;
+                        sum = (sum * 10) + rem;
+                        num = num / 10;
+                    }
+                    if (temp == sum)
+                    {
+                        Console.Write("given num is Palindrome.");
+                    }
+                    else
+                    {
+                        Console.Write("given num is not Palindrome");
+                    }
+                }
+                else
+                {
+                    Console.Write("Enter Your Number To Check Armstrong");
+                    num = int.Parse(Console.ReadLine());
+                    temp = num;
+                    while (num > 0)
+                    {
+                        rem = num % 10;
+                        sum = sum + (rem * rem * rem);
+                        num = num / 10;
+                    }
+
+                    if (temp == sum)
+                    {
+                        Console.WriteLine("the given num is a Armstrong Number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("the given num is not a Armstrong Number");
+                    }
+                }
+                Console.WriteLine("Do you want to continue");
+                abc = Console.ReadLine();
             } while (abc == "yes");
         }
     }
